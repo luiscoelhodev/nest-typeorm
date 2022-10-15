2ea993b3367ac7d125b22c236b3fb8cdab3dea22
Luis Coelho <mancoeluis@gmail.com>
2022-10-14 21:05:30 -0300

    ♻️ simplify variable

--- db/data-source.ts
+++ db/data-source.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: '127.0.0.1' || process.env.MYSQL_HOST,
+  host: process.env.MYSQL_HOST,
   port: 3306,
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
