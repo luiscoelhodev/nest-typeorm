8d1610d3ce34b53f57ca0f2c12c727977789113c
Luis Coelho <mancoeluis@gmail.com>
2022-10-16 00:37:23 -0300

    ✨ add validation whitelist options

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422 }));
+  app.useGlobalPipes(new ValidationPipe({ errorHttpStatusCode: 422, whitelist: true, forbidNonWhitelisted: true }));
   await app.listen(3000);
 }
 bootstrap();
