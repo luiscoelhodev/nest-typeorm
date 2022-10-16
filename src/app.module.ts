1def658cb0f68f8b9f5bd432e64a031bd1c21c9c
Luis Coelho <mancoeluis@gmail.com>
2022-10-16 00:38:27 -0300

    ✨ add posts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProfilesModule } from './profiles/profiles.module';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule, ProfilesModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule, ProfilesModule, PostsModule],
   controllers: [AppController],
   providers: [AppService],
 })
